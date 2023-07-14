fc3abba2b7dd253e05c6307da5e18a8498898193
MaxPower <>
2023-07-14 20:06:08 +0400

    Initial commit

---
+++ RandonAnswer/RandonAnswer/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  RandonAnswer
+//
+//  Created by MaxPower on 10.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var answerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //answerButton.layer.cornerRadius = 10
+    }
+
+    @IBAction func answerButtonAction(_ sender: Any) {
+        let answer = Bool.random()
+        answerLabel.text = ""
+
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+            self.answerLabel.text = answer ? "YES" : "NO"
+        }
+        
+    }
+    
+}
+
